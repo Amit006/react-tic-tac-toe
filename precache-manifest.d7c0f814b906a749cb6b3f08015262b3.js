cb2c6eb2d8788d7475b86baab0dd69953dba12d6
Amit Nayek <amitnk19@gmail.com>
2020-07-23 17:16:23 +0530

    Updates

---
+++ precache-manifest.d7c0f814b906a749cb6b3f08015262b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc7fe4c9cc68689298dc602162dab24",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "17b8a044f66091f6762a",
+    "url": "/react-tic-tac-toe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd72f61b2ae5248a2a5c",
+    "url": "/react-tic-tac-toe/static/js/2.59b8b385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.59b8b385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b8a044f66091f6762a",
+    "url": "/react-tic-tac-toe/static/js/main.f6c32bb1.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
